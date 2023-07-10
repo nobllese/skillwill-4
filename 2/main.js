8b11d38c1bc5d241cafbf7427d4fdedea9236bee
Tamar Jafaridze <116025220+nobllese@users.noreply.github.com>
2023-07-10 16:34:24 +0400

    Update main.js

--- 2/main.js
+++ 2/main.js
@@ -1,12 +1,15 @@
-const newArr = (words) => {
-    const arr1 = words[0].split(' ');
-    return arr1.map((word) => word.toUpperCase()).join(' ');
-  };
-  
-  console.log(newArr([
-    'there was in arabia rostevan, a king by the grace of god, happy, ' +
-    'exalted, generous, modest, lord of many hosts and knights, just and ' +
-    'gracious, powerful, far-seeing, himself a peerless warrior, moreover fluent ' +
-    'in speech: '  //ქართული არა, მაგრამ ინგლისური ვეფხისტყაოსანი გამოგვადგება <3
-  ]));
-  
+const newArr = (sentence) => {
+  return sentence.map((s) => {
+    return s
+      .split(' ')
+      .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+      .join(' ');
+  }).join(' ');
+};
+
+console.log(newArr([
+  'there was in arabia rostevan, a king by the grace of god, happy, ' +
+  'exalted, generous, modest, lord of many hosts and knights, just and ' +
+  'gracious, powerful, far-seeing, himself a peerless warrior, moreover fluent ' +
+  'in speech.'
+]));
